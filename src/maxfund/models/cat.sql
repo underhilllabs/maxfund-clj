81d2f89889fa23e9500368c98aa21ec16c849e5c
Bart Lantz <underhilllabs@gmail.com>
2015-11-19 05:07:02 +0000

    Adds  model for cats

--- src/maxfund/models/cat.sql
+++ src/maxfund/models/cat.sql
@@ -1,5 +1,19 @@
 -- name: find-cat-by-id
-select * from cats
+select * from cat_dms
 where id = :maxfund-id
 limit 1
+
+-- name: find-all-cats
+select * from cat_dms
+
+-- name: find-all-alumni-cats
+select * from cat_dms
+where is_current=0
+order by loc ASC
+
+--name: find-all-current-cats
+select * from cat_dms
+where is_current=1
+order by loc ASC
+
 
